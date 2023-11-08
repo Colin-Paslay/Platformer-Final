32681a07367de22a3322fd415568000c65367183
Colin-Paslay <148103002+Colin-Paslay@users.noreply.github.com>
2023-11-08 10:24:32 -0600

    Player
    Added Player Sprite

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Platformer Final
  Project for my school
+
+
+Player
+Enemies (4)
+Player Death (Including sound effects)
+Enemy Death (Including sound effects)
+Patrolling Enemies
+Pitfalls
+Coins
+Lives
+4 levels
+Start and gameover screens
+breakable blocks and powerups
+new art
---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func _on_start_game_pressed():
+	pass #Place player in level 1 / load saved game?
+
+func _on_level_select_pressed():
+	pass #Let player choose which level they start in
+
+func _on_options_pressed():
+	pass #Take them to an options menu
+
+func _on_quit_pressed():
+	get_tree().quit() #Closes the game
